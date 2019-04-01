4d95841bed662d30cd4fa4b48ef82c035d1eb5c9
Atif Aziz <code@raboof.com>
2019-04-01 16:03:00 +0200

    Remove unused import

--- src/Program.cs
+++ src/Program.cs
@@ -23,7 +23,6 @@ namespace Nod
     using System.IO;
     using System.Linq;
     using System.Reflection;
-    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
     using JavaScriptEngineSwitcher.V8;
